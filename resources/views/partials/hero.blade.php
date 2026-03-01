572c61c4657e38c3ae53afdf9bf4967ac1316660
dcavadiam <diegocamodev@gmail.com>
2026-03-01 11:28:46 -0500

    feat: add hero section with styling and content

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,11 @@
+<section class="hero">
+    <div class="hero__container">
+        <div class="hero__content">
+            <span class="hero__badge">✦ Certificados Oficiales 2026</span>
+            <h1 class="hero__title">Cursos de Psicología con Certificado en 2026</h1>
+            <p class="hero__description">Mira los mejores cursos de psicología para realizar en el 2026. Recibe
+                certificado oficial al finalizar. ¡Conoce Adipa Chile!</p>
+            <a href="#cursos" class="hero__cta">Ver Cursos</a>
+        </div>
+    </div>
+</section>
